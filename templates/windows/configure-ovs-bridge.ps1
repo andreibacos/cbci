27f464fc34df6b8d48f5de9906299d7000a57d1a
gandrei95 <andreign95@gmail.com>
2018-06-08 12:34:12 +0300

    added db parameter for ovs-vsctl

--- templates/windows/configure-ovs-bridge.ps1
+++ templates/windows/configure-ovs-bridge.ps1
@@ -2,7 +2,7 @@ $INTERFACE=(Get-NetIPAddress -AddressFamily ipv4 | Where-Object {$_.IPAddress -m
 $INTERFACE_IP=(Get-NetIPAddress -AddressFamily ipv4 |Where-Object {$_.InterfaceAlias -eq $($INTERFACE)}|Select-Object IPAddress).IPAddress
 Remove-VMSwitch {{ data_bridge }}
 New-VMSwitch -Name {{ data_bridge }} -NetAdapter $INTERFACE -AllowManagementOS $false
-ovs-vsctl add-br {{ data_bridge_ovs }}
-ovs-vsctl add-port {{ data_bridge_ovs }} $INTERFACE
+ovs-vsctl --db=tcp:127.0.0.1:6640 add-br {{ data_bridge_ovs }}
+ovs-vsctl --db=tcp:127.0.0.1:6640 add-port {{ data_bridge_ovs }} $INTERFACE
 Enable-NetAdapter {{ data_bridge_ovs }}
 New-NetIPAddress -IPAddress $INTERFACE_IP -InterfaceAlias {{ data_bridge_ovs }} -PrefixLength 22
