147ff56d782313536523b94b4cdee4243940bdd3
gandrei95 <andreign95@gmail.com>
2018-06-08 12:39:19 +0300

    minor change

--- templates/windows/configure-ovs-bridge.ps1
+++ templates/windows/configure-ovs-bridge.ps1
@@ -2,6 +2,7 @@ $INTERFACE=(Get-NetIPAddress -AddressFamily ipv4 | Where-Object {$_.IPAddress -m
 $INTERFACE_IP=(Get-NetIPAddress -AddressFamily ipv4 |Where-Object {$_.InterfaceAlias -eq $($INTERFACE)}|Select-Object IPAddress).IPAddress
 Remove-VMSwitch {{ data_bridge }}
 New-VMSwitch -Name {{ data_bridge }} -NetAdapter $INTERFACE -AllowManagementOS $false
+Get-VMSwitchExtension -VMSwitchName {{ data_bridge }} -Name "Cloudbase Open vSwitch Extension"
 ovs-vsctl --db=tcp:127.0.0.1:6640 add-br {{ data_bridge_ovs }}
 ovs-vsctl --db=tcp:127.0.0.1:6640 add-port {{ data_bridge_ovs }} $INTERFACE
 Enable-NetAdapter {{ data_bridge_ovs }}
