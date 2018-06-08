bda557ef41795cbcc4b26171a135c4c91da456db
gandrei95 <andreign95@gmail.com>
2018-06-08 12:33:19 +0300

    added ovs script

--- templates/windows/configure-ovs-bridge.ps1
+++ templates/windows/configure-ovs-bridge.ps1
@@ -1,7 +1,8 @@
 $INTERFACE=(Get-NetIPAddress -AddressFamily ipv4 | Where-Object {$_.IPAddress -match "192.168.0"}|Select-Object InterfaceAlias).InterfaceAlias
 $INTERFACE_IP=(Get-NetIPAddress -AddressFamily ipv4 |Where-Object {$_.InterfaceAlias -eq $($INTERFACE)}|Select-Object IPAddress).IPAddress
-ovs-vsctl add-br {{ data_bridge }}
-ovs-vsctl del-br {{ data_bridge }}
-ovs-vsctl add-port {{ data_bridge }} $INTERFACE
-Enable-NetAdapter {{ data_bridge }}
-New-NetIPAddress -IPAddress $INTERFACE_IP -InterfaceAlias {{ data_bridge }} -PrefixLength 22
+Remove-VMSwitch {{ data_bridge }}
+New-VMSwitch -Name {{ data_bridge }} -NetAdapter $INTERFACE -AllowManagementOS $false
+ovs-vsctl add-br {{ data_bridge_ovs }}
+ovs-vsctl add-port {{ data_bridge_ovs }} $INTERFACE
+Enable-NetAdapter {{ data_bridge_ovs }}
+New-NetIPAddress -IPAddress $INTERFACE_IP -InterfaceAlias {{ data_bridge_ovs }} -PrefixLength 22
