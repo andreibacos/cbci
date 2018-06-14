bd70eb0539bf00e3ff42b29338cf40e3b36aa213
gandrei95 <andreign95@gmail.com>
2018-06-14 09:52:52 +0300

    os-brick integration with OVS neutron agent

--- templates/windows/configure-ovs-bridge.ps1
+++
@@ -1,9 +0,0 @@
-Remove-VMSwitch {{ data_bridge }}
-$INTERFACE=(Get-NetIPAddress -AddressFamily ipv4 | Where-Object {$_.IPAddress -match "192.168.0"}|Select-Object InterfaceAlias).InterfaceAlias
-$INTERFACE_IP=(Get-NetIPAddress -AddressFamily ipv4 |Where-Object {$_.InterfaceAlias -eq $($INTERFACE)}|Select-Object IPAddress).IPAddress
-New-VMSwitch -Name {{ data_bridge }} -NetAdapterName $INTERFACE -AllowManagementOS $false
-Enable-VMSwitchExtension -VMSwitchName {{ data_bridge }} -Name "Cloudbase Open vSwitch Extension"
-ovs-vsctl --db=tcp:127.0.0.1:6640 add-br {{ data_bridge_ovs }}
-ovs-vsctl --db=tcp:127.0.0.1:6640 add-port {{ data_bridge_ovs }} $INTERFACE
-Enable-NetAdapter {{ data_bridge_ovs }}
-New-NetIPAddress -IPAddress $INTERFACE_IP -InterfaceAlias {{ data_bridge_ovs }} -PrefixLength 22
